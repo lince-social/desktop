1d3f66146f796a6b02fa23fe5eeef0a11b13d828
xaviduds <xaviduds@gmail.com>
2025-01-29 10:34:21 -0300

    started structure

--- language-servers.nix
+++ language-servers.nix
@@ -5,7 +5,13 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     # postgresql_17
     zig
+    zls
+    nixfmt-rfc-style
+    nil
     lua
+    marksman
+    markdown-oxide
+    tailwindcss-language-server
   ];
 
   shellHook = ''
